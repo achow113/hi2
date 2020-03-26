98648cfc5fe09730b621d281b0fade6cb6616733
A Chow <achow113@hotmail.com>
2020-03-26 23:01:53 +0800

    basic version

---
+++ retrypw.py
@@ -0,0 +1,16 @@
+password = 'a123456'
+n = 3 # 輸入次數機會
+while n > 0:
+	pwd = input('請輸入密碼:')
+	if pwd == password:
+		print('登入成功')
+		break # stop loop
+	else :
+		n = n - 1
+		print('密碼錯誤!還有', n , '次機會')
+
+			
+
+
+
+	
